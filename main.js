b0783ed35361f3df88088e7b8a5b80c2a5e78b92
TheBigH50 <HFangmeier@outlook.com>
2022-08-24 14:23:10 -0500

    prettier

--- main.js
+++ main.js
@@ -2,19 +2,30 @@ console.log("Script attached");
 /*----------- Exercise #1: SELECTING/MANIPULATING ELEMENTS -----------*/
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
-const getNode1 = document.getElementById("node1"); getNode1.innerHTML = 'I used the getElementById("node1") method to access this'
+const getNode1 = document.getElementById("node1");
+getNode1.innerHTML = 'I used the getElementById("node1") method to access this';
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
-const getNode2 = document.getElementsByClassName("node2"); getNode2[0].innerHTML = `I used the getElementByClassName("node2") method to access this`
+const getNode2 = document.getElementsByClassName("node2");
+getNode2[0].innerHTML = `I used the getElementByClassName("node2") method to access this`;
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
-const getH3Tags = document.getElementsByTagName("h3"); for (let ele of getH3Tags) {ele.innerHTML = `I used the getElementByTagName("h3") method to access all of these`;}
+const getH3Tags = document.getElementsByTagName("h3");
+for (let ele of getH3Tags) {
+  ele.innerHTML = `I used the getElementByTagName("h3") method to access all of these`;
+}
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
-const newParagraph1 = document.createElement("p"); const paraContent1 = document.createTextNode(`This node was created using the createElement() method`);
+const newParagraph1 = document.createElement("p");
+const paraContent1 = document.createTextNode(
+  `This node was created using the createElement() method`
+);
 // TODO: Append the created node to the parent node using the element.appendChild() method
-newParagraph1.appendChild(paraContent1); const newP = document.getElementById("parent"); newP.appendChild(newParagraph1);
+newParagraph1.appendChild(paraContent1);
+const newP = document.getElementById("parent");
+newP.appendChild(newParagraph1);
 // TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-const newA = document.createElement("a"); newA.innerHTML = `I am a <a href="https://geeksforgeeks.org">&lt;a&gt;</a> tag`;
+const newA = document.createElement("a");
+newA.innerHTML = `I am a <a href="https://geeksforgeeks.org">&lt;a&gt;</a> tag`;
 // BONUS: Add a link href to the <a>
 //newA.title = "I am a <a> tag"; newA.href = "https://www.geeksforgeeks.org"; newP.appendChild(newA);
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
@@ -28,8 +39,18 @@ newP.insertBefore(newA, newParagraph1);
 /*----------- Exercise #4: LIST ITEMS ----------- */
 // Use the following array of values to generate a list on the DOM
 
-let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
-
+let list = [
+  "apples",
+  "bananas",
+  "carrots",
+  "dragon fruit",
+  "eggplant",
+  "fish",
+  "grapes",
+  "honey",
+  "ice bag",
+  "juice (any kind)",
+];
 
 // TODO: Create an unordered list element
 
@@ -37,7 +58,7 @@ let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish",
 
 // TODO: Append the new list items to the unordered list element
 
-// TODO: Append the unordered list to the `div#container` under exercise 4 
+// TODO: Append the unordered list to the `div#container` under exercise 4
 
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
