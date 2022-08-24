174aaddac695c0df0c7e52b56c906829a0138563
TheBigH50 <HFangmeier@outlook.com>
2022-08-24 14:07:03 -0500

    finish Exe2

--- main.js
+++ main.js
@@ -14,11 +14,11 @@ const newParagraph1 = document.createElement("p"); const paraContent1 = document
 // TODO: Append the created node to the parent node using the element.appendChild() method
 newParagraph1.appendChild(paraContent1); const newP = document.getElementById("parent"); newP.appendChild(newParagraph1);
 // TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
-const newA = document.createElement("a"); newA.innerHTML = `I am a <a href="https://geeksforgeeks.org">&lt;a&gt;</a> tag`; newP.appendChild(newA);
+const newA = document.createElement("a"); newA.innerHTML = `I am a <a href="https://geeksforgeeks.org">&lt;a&gt;</a> tag`;
 // BONUS: Add a link href to the <a>
 //newA.title = "I am a <a> tag"; newA.href = "https://www.geeksforgeeks.org"; newP.appendChild(newA);
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
-
+newP.insertBefore(newA, newParagraph1);
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
